5d0b9c2512051e8f95f020217ba6eac27ee46347
Thibz <thibault.sanglier2005@gmail.com>
2025-11-20 17:36:12 +0100

    Config: ajout configuration applu + connexion BDD (PDO)

---
+++ backend/config/config.php
@@ -0,0 +1,11 @@
+<?php
+// backend/config/config.php
+
+define('APP_DEBUG', true);
+
+// Paramètres de connexion BDD
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'fitness_db');
+define('DB_USER', 'root');
+define('DB_PASS', ''); // vide avec Laragon
+define('DB_CHARSET', 'utf8mb4');
